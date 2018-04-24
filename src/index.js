0438b7e271ad674cd2fa3e95d8d4bc384afe8ae2
Ben Zuill-Smith <bzuillsmith@gmail.com>
2018-04-24 12:17:32 -0700

    More progress toward getting node oidc server working. Having an issue with the token endpoint at the moment which is probably a configuration problem.

--- src/index.js
+++ src/index.js
@@ -8,23 +8,35 @@ const nunjucks = require('nunjucks');
 const querystring = require('querystring');
 const path = require('path');
 const bodyParser = require('body-parser');
+const http = require('http');
+const https = require('https');
+const fs = require('fs');
+const rewrite = require('express-urlrewrite');
 
 const ROOT_URL = '/connect';
 
 
-//const configuration = { features: { pkce: { forcedForNative: true } } };
+// const configuration = { features: { pkce: { forcedForNative: true } } };
 process.on('unhandledRejection', error => {
     logger.error(error);
 });
 
 const app = express();
+app.proxy = true;
 
 nunjucks.configure(path.join(__dirname, 'views'), {
     autoescape: true,
     express: app
 });
 
-const oidc = new Provider('http://localhost', {
+app.get('/', function(req, res){
+    res.send('<DOCTYPE html><html><body>Hello World!</body></html>');
+});
+
+const prefix = '/connect';
+app.use(rewrite('/.well-known/*', `${prefix}/.well-known/$1`));
+
+const oidc = new Provider('http://192.168.1.114:8080', {
     claims: {
         email: ['email', 'email_verified'],
         phone: ['phone_number', 'phone_number_verified'],
@@ -47,6 +59,17 @@ const oidc = new Provider('http://localhost', {
         //request: true, // defaults to false
         //revocation: true, // defaults to false
         //sessionManagement: true, // defaults to false
+    },
+    routes: {
+        authorization: '/auth',
+        certificates: '/certs',
+        check_session: '/session/check',
+        end_session: '/session/end',
+        introspection: '/token/introspection',
+        registration: '/reg',
+        revocation: '/token/revocation',
+        token: '/token',
+        userinfo: '/me'
     }
 });
 
@@ -57,6 +80,10 @@ oidc.initialize({ adapter: MongoAdapter })
         app.use(ROOT_URL, oidc.callback);
         app.use(bodyParser.json());
         app.use(bodyParser.urlencoded({ extended: false }));
+
+        app.get('/', function(req, res){
+            res.send('<h1>Hello world!</h1>');
+        });
 
         app.get('/interaction/:grant', async (req, res, next) => {
             try{
@@ -103,13 +130,28 @@ oidc.initialize({ adapter: MongoAdapter })
             var account = await users.findByEmail(req.body.login);
 
             if(!account) {
-                //TODO: handle invalid login
-                return res.send('invalid login');
+                // get the oidc session
+                const details = await oidc.interactionDetails(req);
+                // get the authorized client that requested the interaction
+                const client = await oidc.Client.find(details.params.client_id);
+                res.render('signin.html', {
+                    client,
+                    details,
+                    title: 'Sign-in',
+                    errorMessage: 'Invalid email or password.',
+                    debug: querystring.stringify(details.params, ',<br/>', ' = ', {
+                        encodeURIComponent: value => value,
+                    }),
+                    interaction: querystring.stringify(details.interaction, ',<br/>', ' = ', {
+                        encodeURIComponent: value => value,
+                    }),
+                });
+                return;
             }
 
             const result = {
                 login: {
-                    account: account.accountId,
+                    account: account.id,
                     acr: 'urn:mace:incommon:iap:bronze',
                     amr: ['pwd'],
                     remember: !!req.body.remember,
@@ -117,12 +159,28 @@ oidc.initialize({ adapter: MongoAdapter })
                 },
                 consent: {},
             };
-            res.send(result);
-            //await oidc.interactionFinished(req, res, result);
-            //await next();
+            //res.send(result);
+            await oidc.interactionFinished(req, res, result);
+            await next();
         });
 
-        app.listen(80);
+        app.get('/redirect', function(req, res) {
+            logger.info('redirected to native client');
+        });
+
+        app.post('/interaction/:grant/confirm', async (req, res, next) => {
+            const result = { consent: {} };
+            await oidc.interactionFinished(req, res, result);
+            await next();
+        });
+
+        // var options = {
+        //     pfx: fs.readFileSync(path.join(__dirname, 'dev-tls.pfx')),
+        //     passphrase: 'N9L7ePSNMSCZyqm3TryRpUkj'
+        // };
+
+        //https.createServer(options, app).listen(8081);
+        http.createServer(app).listen(8080);
     })
     .catch(function(err) {
         logger.error(err);
--- src/users.js
+++ src/users.js
@@ -1,3 +1,4 @@
+exports.findById = findById;
 exports.findByEmail = findByEmail;
 
 const users = [
@@ -5,27 +6,36 @@ const users = [
         id: '12345abcdef',
         given_name: 'Ben',
         family_name: 'Z',
-        email: 'ben@__fake__.com',
+        email: 'ben@__fake__.com'
     },
     {
         id: 'abcdef12345',
         given_name: 'Ron',
         family_name: 'S',
-        email: 'ron@__fake__.com',
+        email: 'ron@__fake__.com'
     }
 ];
 
 async function findByEmail(email) {
+    return findUser(u => u.email == email);
+}
+
+async function findById(req, id) {
+    return findUser(u => u.id == id);
+}
+
+function findUser(fn) {
     var theUser = null;
     for(var user of users) {
-        if(user.email === email) {
+        if(fn(user)) {
             theUser = user;
             break;
         }
     }
     
-    return {
-        accountId: theUser.id,
-        async claims() { return { sub: user.id }; },
-    };
+    if (theUser == null) return null;
+    
+    return Object.assign({}, theUser, {
+        sub: this.accountId,
+    });
 }
